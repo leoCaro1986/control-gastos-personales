b998471fdb2c2e52ac09d63cff386899bbb0937f
Leonardo <leocv0205@gmail.com>
2023-03-20 21:56:39 -0500

    Se realiza validación de formulario

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,38 @@
-import React from 'react'
+import { useState } from 'react'
+import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto, setIsValidPresupuesto}) => {
+
+    const [mensaje, setMensaje] = useState("")
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if (!presupuesto || presupuesto < 0) {
+            setMensaje("No es un presupuesto valido");
+
+            return
+        }
+        setMensaje("")
+        setIsValidPresupuesto(true);
+    }
+
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>Definir Presupuesto</label>
                 <input
                     className='nuevo-presupuesto'
-                    type="text"
+                    type="number"
                     placeholder="Añade Presupuesto"
+                    value={presupuesto}
+                    onChange={e => setPresupuesto(Number(e.target.value))}
                 />
             </div>
             <input type="submit" value="Añadir"/>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
 
         </form>
     </div>
